0addb6c1313d883fe6e6211510bb4f2c264888ae
L0002623\vector <layala@vectoritcgroup.com>
2018-11-09 19:01:39 -0500

    add project

---
+++ app/src/main/java/ghostl/com/photofeed/PhotoFeedActivity.java
@@ -0,0 +1,13 @@
+package ghostl.com.photofeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_feed);
+    }
+}
